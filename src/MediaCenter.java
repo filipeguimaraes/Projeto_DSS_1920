13e772691d526c6fff456cfac089f552d54635bf
Filipe Guimarães <filipeguimaraes@users.noreply.github.com>
2019-10-14 10:34:40 +0100

    for goncas

--- src/MediaCenter.java
+++ src/MediaCenter.java
@@ -16,11 +16,15 @@ public class MediaCenter extends Application {
         Image image = new Image("/images/icon.png");
         stage.getIcons().add(image);
 
-        stage.setScene( new Scene(FXMLLoader.load(getClass().getResource("Views/FirstMenu.fxml")),600,400));
-        //stage.setScene( new Scene(FXMLLoader.load(getClass().getResource("Views/Login.fxml")),600,400));
-        //stage.setScene( new Scene(FXMLLoader.load(getClass().getResource("Views/AdminLogin.fxml")),600,400));
-        //stage.setScene( new Scene(FXMLLoader.load(getClass().getResource("Views/ErrorBox.fxml")),400,200));
+        Scene FirstMenu = new Scene(FXMLLoader.load(getClass().getResource("Views/FirstMenu.fxml")),600,400);
+        Scene Login = new Scene(FXMLLoader.load(getClass().getResource("Views/Login.fxml")),600,400);
+        Scene AdminLogin = new Scene(FXMLLoader.load(getClass().getResource("Views/AdminLogin.fxml")),600,400);
+        Scene ErrorBox = new Scene(FXMLLoader.load(getClass().getResource("Views/ErrorBox.fxml")),400,200);
+        Scene MainPage = new Scene(FXMLLoader.load(getClass().getResource("Views/MainPage.fxml")));
 
+
+        stage.setScene(MainPage);
+        if(stage.getScene().equals(MainPage)) stage.setMaximized(true);
         stage.show();
     }
 }
